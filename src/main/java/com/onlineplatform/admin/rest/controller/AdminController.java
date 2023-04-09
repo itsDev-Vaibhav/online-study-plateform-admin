4bef860a1489b2dcc872ab07c9e055ef81d9cdd6
itsVaibhav91 <vaibhavbhatnagar96@gmail.com>
2023-04-09 16:04:42 +0530

    controller changes

---
+++ src/main/java/com/onlineplatform/admin/rest/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.onlineplatform.admin.rest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/project/v1")
+public class AdminController {
+	
+	
+	@GetMapping("/getMsg")
+	public ResponseEntity<String> showMessage() {
+		return new ResponseEntity<String>("Hi this is response from admin Controller.", HttpStatus.OK);
+	}
+
+}
