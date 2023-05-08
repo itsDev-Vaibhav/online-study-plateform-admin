08e0e7db06a7cf023472641b55c786fc4ae645d5
itsVaibhav91 <vaibhavbhatnagar96@gmail.com>
2023-05-09 00:21:04 +0530

    new db changes

--- src/main/java/com/onlineplatform/admin/db/entities/Roles.java
+++ src/main/java/com/onlineplatform/admin/db/entities/Roles.java
@@ -1,18 +1,53 @@
 package com.onlineplatform.admin.db.entities;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Roles {
 
 	private Long roleId;
 	private String roleName;
-	
-	
-	
+
+	private Set<Users> users = new HashSet<>();
+
+	public Roles() {
+
+	}
+
+	public Roles(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public Long getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public Set<Users> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<Users> users) {
+		this.users = users;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(roleId, roleName);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -24,7 +59,10 @@ public class Roles {
 		Roles other = (Roles) obj;
 		return Objects.equals(roleId, other.roleId) && Objects.equals(roleName, other.roleName);
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "Roles [roleId=" + roleId + ", roleName=" + roleName + "]";
+	}
+
 }
--- src/main/java/com/onlineplatform/admin/db/entities/Users.java
+++ src/main/java/com/onlineplatform/admin/db/entities/Users.java
@@ -1,6 +1,8 @@
 package com.onlineplatform.admin.db.entities;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Users {
 	
@@ -9,7 +11,79 @@ public class Users {
 	private String email;
 	private String password;
 	
+	private Set<Roles> roles = new HashSet<>();
 	
+	private Students student;
+	
+	private Instructor instructor;
+	
+	
+	
+	
+	
+	
+	public Users() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Users(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Roles> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Roles> roles) {
+		this.roles = roles;
+	}
+
+	public Students getStudent() {
+		return student;
+	}
+
+	public void setStudent(Students student) {
+		this.student = student;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(email, password, userId);
@@ -27,5 +101,15 @@ public class Users {
 		return Objects.equals(email, other.email) && Objects.equals(password, other.password)
 				&& Objects.equals(userId, other.userId);
 	}
+
+
+
+	@Override
+	public String toString() {
+		return "Users [userId=" + userId + ", email=" + email + ", password=" + password + "]";
+	}
+	
+	
+	
 
 }
